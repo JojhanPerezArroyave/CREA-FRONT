f3abf851e91edff45b2f36e2ab489d7bbbec9eb7
Erika <erika.orregog@opitech.com.co>
2024-10-19 11:22:02 -0500

    feat: using item list component

--- src/app/modules/classrooms/container/classroom-list-container/classroom-list-container.component.ts
+++ src/app/modules/classrooms/container/classroom-list-container/classroom-list-container.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ClassroomItemListComponent } from '../../ui/classroom-item-list/classroom-item-list.component';
 
 @Component({
   selector: 'app-classroom-list-container',
   standalone: true,
   templateUrl: './classroom-list-container.component.html',
+  imports: [ClassroomItemListComponent],
   styleUrls: ['./classroom-list-container.component.scss'],
 })
 export class ClassroomListContainerComponent  implements OnInit {
+
+  classroom = {
+    name: 'D302 Edificio del parque',
+    teacher: 'Mario Alejandro Perez',
+    status: 'Disponible',
+    date: '6pm'
+  }
 
   constructor() { }
 
