b85290728ab538074f9c3f708e555f2fcc275fea
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-10-29 14:07:34 -0500

    chore: add server configuration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+
+import { JsonPipe } from '@angular/common';
+import { GetClassroomService } from './home.repository';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, JsonPipe],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  private readonly getClassroomService = inject(GetClassroomService);
+  data = [];
+
+  ngOnInit(): void {
+    this.getClassroomService.getDatos().subscribe((data) => {
+      this.data = data;
+    });
+  }
 }
---
+++ src/app/home/home.repository.ts
@@ -0,0 +1,16 @@
+// nombre-del-servicio.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetClassroomService {
+  constructor(private http: HttpClient) {}
+
+  getDatos(): Observable<any> {
+    return this.http.get<any>(`${environment.API_URL}/Classroom`); // Cambia <any> al tipo de datos que esperas recibir
+  }
+}
