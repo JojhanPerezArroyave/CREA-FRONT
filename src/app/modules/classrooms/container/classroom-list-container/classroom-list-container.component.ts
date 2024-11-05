b7f8a5d693c052ad2e15c1de4e834e81c4f5bee7
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-11-05 16:29:41 -0500

    chore: git merge with Erika branch

--- src/app/modules/classrooms/container/classroom-list-container/classroom-list-container.component.ts
+++ src/app/modules/classrooms/container/classroom-list-container/classroom-list-container.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ClassroomItemListComponent } from '../../ui/classroom-item-list/classroom-item-list.component';
+import { GetClassroomService } from '../../services/classroom.service';
+import { JsonPipe } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-classroom-list-container',
   standalone: true,
   templateUrl: './classroom-list-container.component.html',
-  imports: [ClassroomItemListComponent],
+  imports: [
+    ClassroomItemListComponent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    JsonPipe,
+  ],
   styleUrls: ['./classroom-list-container.component.scss'],
 })
-export class ClassroomListContainerComponent  implements OnInit {
+export class ClassroomListContainerComponent implements OnInit {
+  private readonly getClassroomService = inject(GetClassroomService);
+  data = [];
 
   classroom = {
     name: 'D302 Edificio del parque',
     teacher: 'Mario Alejandro Perez',
     status: 'Disponible',
-    date: '6pm'
+    date: '6pm',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getClassroomService.getDatos().subscribe((data) => {
+      this.data = data;
+    });
   }
-
-  constructor() { }
-
-  ngOnInit() {}
-
 }
